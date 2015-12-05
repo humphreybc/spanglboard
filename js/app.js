08768d4096c45ce64a28f017245e7b49b1e7814c
Benjamin Humphrey <humphreybc@gmail.com>
2015-12-05 17:25:44 +1100

    Remove jQuery

--- js/app.js
+++ js/app.js
@@ -1,66 +1,74 @@
-$(document).ready(function() {
+var body = document.body;
+var audioEls = document.getElementsByClassName("audio");
 
-  var body = $("body");
+addClickHandlerToAudio();
+addClickHandlerToRandomMode();
 
-  function getAudio(id) {
-    return document.getElementById(id + "-audio");
+function getAudio(id) {
+  return document.getElementById(id + "-audio");
+}
+
+function getLength(a) {
+  return a.duration;
+}
+
+function playAudio(id) {
+  var a = getAudio(id);
+  var length = getLength(a);
+  showProgress(id, length);
+  a.play();
+  return length;
+}
+
+function toggleProgressBar(id, transition) {
+  id = document.getElementById(id);
+  var progressEl = id.children[0];
+  progressEl.setAttribute("style", transition);
+  id.classList.toggle("playing", transition);
+}
+
+function showProgress(id, length) {
+  var transition = "transition: width " + length + "s linear;"
+  toggleProgressBar(id, transition);
+
+  setTimeout(function() {
+    toggleProgressBar(id, false);
+  }, length * 1000);
+}
+
+function randomSoundbite() {
+  var number = Math.floor(Math.random() * audioEls.length);
+  return audioEls[number];
+}
+
+function randomMode() {
+  var id = randomSoundbite().id;
+  var length = playAudio(id);
+
+  setTimeout(function() {
+    if (! body.classList.contains("random")) {
+      return; 
+    }
+    randomMode();
+  }, length * 900);
+}
+
+function addClickHandlerToAudio() {
+  for (var i = 0; i < audioEls.length; i++) {
+    audioEls[i].onclick = function() {
+      playAudio(this.id);
+    };
   }
+}
 
-  function getLength(a) {
-    return a.duration;
-  }
-
-  function playAudio(id) {
-    var a = getAudio(id);
-    var length = getLength(a);
-    showProgress(id, length);
-    a.play();
-    return length;
-  }
-
-  function toggleProgressBar(id, transition) {
-    $(id).find(".progress").attr("style", transition);
-    $(id).toggleClass("playing", transition);
-  }
-
-  function showProgress(id, length) {
-    var id = "#" + id;
-    var transition = "transition: width " + length + "s linear;"
-    toggleProgressBar(id, transition);
-
-    setTimeout(function() {
-      toggleProgressBar(id, false);
-    }, length * 1000);
-  }
-
-  function randomSoundbite() {
-    var soundbites = $(".audio");
-    var number = Math.floor(Math.random() * soundbites.length);
-    return soundbites[number];
-  }
-
-  function randomMode() {
-    var id = randomSoundbite().id;
-    var length = playAudio(id);
-
-    setTimeout(function() {
-      if (! body.hasClass("random")) {
-        return; 
-      }
-      randomMode();
-    }, length * 900);
-  }
-
-  $(".audio").on("click", function() {
-    playAudio(this.id);
-  });
-
-  $("#random-mode").on("click", function() {
-    if (body.hasClass("random")) {
-      body.removeClass("random");
+function addClickHandlerToRandomMode() {
+  var randomModeEl = document.getElementById("random-mode");
+  randomModeEl.onclick = function() {
+    if (body.classList.contains("random")) {
+      body.className = "";
     } else {
-      body.addClass("random");
+      body.className = "random";
       randomMode();
     }
-  });
-});
+  };
+}
