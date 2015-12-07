f4482e3a5a2bf842fd6261c3e8f134522c24c16e
Benjamin Humphrey <humphreybc@gmail.com>
2015-12-07 21:19:18 +1100

    Populate the DOM using JS instead, and add a few more soundbites

--- js/app.js
+++ js/app.js
@@ -1,6 +1,41 @@
 var body = document.body;
 var audioEls = document.getElementsByClassName("audio");
+var soundbitesEl = document.getElementById("soundbites");
+var audioFilesEl = document.getElementById("audio-files");
 
+var soundbites = {"atlassian":"At Atlassian", 
+                  "confused":"I’m getting confused again",
+                  "whoops":"Whoops",
+                  "simplicity-far-side":"Simplicity on the far side of complexity",
+                  "sidebar":"Sidebar",
+                  "design-serious":"We take design serious",
+                  "laughter":"(Laughter)",
+                  "grumpy":"I have a grumpy resting face",
+                  "fuck-1":"Fuck 1",
+                  "fuck-2":"Fuck 2",
+                  "fuck-3":"Fuck 3",
+                  "aim-to-design":"Our aim is to design",
+                  "gibberish-1":"Gibberish 1",
+                  "gibberish-2":"Gibberish 2",
+                  "discovered":"We discovered",
+                  "clear-throat":"(Clears throat)",
+                  "bam":"Bam bam bam",
+                  "right":"Right, yes, yep, yeah",
+                  "jony-ive":"I don’t need to talk as slow as Jony Ive",
+                  "uhm":"Uhm",
+                  "simplifying":"Simplifying is freakin’ hard",
+                  "jira":"JIRA 7",
+                  "really":"Really?",
+                  "flow":"Being in the flow",
+                  "design-pm-teams":"Our design, PM&hellip;",
+                  "effortless":"Create an effortless &amp; harmonious&hellip;",
+                  "to-do-this":"To do this",
+                  "hunga":"Do you have the hunga",
+                  "really-understand-customers":"When we really understand&hellip;",
+                  "whole-thing":"I try the whole thing otherwise it doesn’t work"
+                  }
+
+populateContent();
 addClickHandlerToAudio();
 addClickHandlerToRandomMode();
 loadAudioFiles();
@@ -52,6 +87,14 @@ function randomMode() {
     }
     randomMode();
   }, length * 900);
+}
+
+function populateContent() {
+  for (var key in soundbites) {
+    soundbitesEl.insertAdjacentHTML('beforeend', '<button id=' + key + ' class="box audio">' + soundbites[key] + '<span class="progress"></span></button>');
+    audioFilesEl.insertAdjacentHTML('beforeend', '<audio id=' + key + '-audio' + ' src="assets/mp3/' + key + '.mp3" preload="auto" type="audio/mp3"></audio>');
+  }
+  soundbitesEl.insertAdjacentHTML('beforeend', '<button id="random-mode" class="box" title="Shuffler 9000™"><span id="random-mode-icon"></span></button>');
 }
 
 function addClickHandlerToAudio() {
